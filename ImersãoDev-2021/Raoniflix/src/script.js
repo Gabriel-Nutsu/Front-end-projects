b27f3288007b59545c7609fb57c5fa9c5ce74c32
Gabriel Moraes <51194705+Gabriel-Nutsu@users.noreply.github.com>
2021-09-18 18:39:08 -0300

    Add files via upload

---
+++ ImersãoDev-2021/Raoniflix/src/script.js
@@ -0,0 +1,10 @@
+var listaFilmes = [
+  "https://capricho.abril.com.br/wp-content/uploads/2019/12/brightplaces.jpg?quality=85&strip=info",
+  "https://i2.wp.com/www.otakupt.com/wp-content/uploads/2020/09/Horimiya-vai-ter-serie-anime-1.jpg?resize=696%2C1045&ssl=1",
+  "https://br.web.img3.acsta.net/pictures/21/05/26/20/19/2966501.jpg",
+  "https://br.web.img3.acsta.net/medias/nmedia/18/95/59/60/20417256.jpg"
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
